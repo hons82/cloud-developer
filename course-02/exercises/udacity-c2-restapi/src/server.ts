c8910e649980058bae5709084bd1894209ec3a25
Hannes Tribus <hannes@vertical-life.info>
2020-05-02 21:03:32 +0200

    Fix CORS problems

--- course-02/exercises/udacity-c2-restapi/src/server.ts
+++ course-02/exercises/udacity-c2-restapi/src/server.ts
@@ -22,9 +22,10 @@ import { V0MODELS } from './controllers/v0/model.index';
 
   //CORS Should be restricted
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+      next();
   });
 
   app.use('/api/v0/', IndexRouter)
